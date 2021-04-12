fb583e1211be669300c833f0e9d6eb5d6fdf9ed3
Santhosh GL <santhosh.gl@synduit.com>
2021-04-12 15:17:43 +0530

    Reverting to yarn

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
 const app = express()
 
-//here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist/spa')))
-
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/spa/index.html'))
-})
-
-const port = process.env.PORT || 8080
+app.use(history())
+app.use(serveStatic(__dirname + '/dist/spa'))
 app.listen(port)
-console.log(`app is listening on port: ${port}`)
-
